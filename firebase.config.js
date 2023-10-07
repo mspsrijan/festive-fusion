bcd95e65060117cf5e35b2558be1b5da1b612254
Shah Paran Srijan <mspsrijan@gmail.com>
2023-10-07 14:36:39 +0600

    Project initialized with Vite, React, React Router, Firebase and TailwindCSS

---
+++ firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZebMh-FFIjCYHzHcXudEi3sOt2sJriWE",
+  authDomain: "festive-fusion.firebaseapp.com",
+  projectId: "festive-fusion",
+  storageBucket: "festive-fusion.appspot.com",
+  messagingSenderId: "453607259624",
+  appId: "1:453607259624:web:af339fb3012cda5ed5f85d"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
