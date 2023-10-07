23ccbc1cb49957e2fc39dad88bd4ee17c738bff5
Shah Paran Srijan <mspsrijan@gmail.com>
2023-10-07 14:50:10 +0600

    Configured env variables

--- firebase.config.js
+++ firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZebMh-FFIjCYHzHcXudEi3sOt2sJriWE",
-  authDomain: "festive-fusion.firebaseapp.com",
-  projectId: "festive-fusion",
-  storageBucket: "festive-fusion.appspot.com",
-  messagingSenderId: "453607259624",
-  appId: "1:453607259624:web:af339fb3012cda5ed5f85d"
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
