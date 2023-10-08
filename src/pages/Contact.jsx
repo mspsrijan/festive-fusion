4bba4aeb7eecd0175e1878e6b1698af7a367dfe8
Shah Paran Srijan <mspsrijan@gmail.com>
2023-10-08 21:07:24 +0600

    Designed single service page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,30 @@
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
+      <div>
+        <div
+          className="hero min-h-[350px] px-5"
+          style={{
+            backgroundImage: "url(https://i.ibb.co/G7dhHsd/contact-bg.jpg)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundColor: "rgba(0,0,0,0.6)",
+            backgroundBlendMode: "overlay",
+          }}
+        >
+          <div className="text-center text-white">
+            <div className="max-w-[800px]" data-aos="fade-in">
+              <h1 className="mb-5 text-4xl lg:text-5xl font-extrabold leading-snug">
+                Get in Touch
+              </h1>
+              <p className="mb-5">
+                Reach out to us today. We'd love to hear from you and help bring
+                your event dreams to life.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
